fed6a2097c55f0f3751aa5670aebd365b7107e11
Keran Barbieri <keranbarbieri@hotmail.com>
2022-01-14 10:50:36 -0300

    _redirects

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -20,7 +20,7 @@ function MovieCard() {
                 <h3>{movie.title}</h3>
                 <MovieScore />
                 <Link to={`/form/${movie.id}`}>
-                    <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+                    <div className="dsmovie-btn">Avaliar</div>
                 </Link>
 
             </div>
--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -32,11 +32,11 @@ function Form() {
                         </select>
                     </div>
                     <div className="dsmovie-form-btn-container">
-                        <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
+                        <button type="submit" className="dsmovie-btn">Salvar</button>
                     </div>
                 </form >
                 <Link to="/">
-                    <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                    <button className="dsmovie-btn mt-3">Cancelar</button>
                 </Link>
             </div >
         </div >
